89ec7903c533488edd07812c31e352532dc3cc12
Terma <termafaxplay@outlook.com>
2024-05-30 22:42:18 -0300

    Optimized practice sprite on progressbar feature

--- src/ui/game/progress/progress_icon.cpp
+++ src/ui/game/progress/progress_icon.cpp
@@ -23,9 +23,17 @@ void PlayerProgressIcon::updateIcons(const PlayerIconData& data) {
     auto color2 = gm->colorForIdx(data.color2);
 
     Build<CCLayerColor>::create(ccc4(color1.r, color1.g, color1.b, 255), 2.f, 8.f)
-        .pos(0.f, 3.f)
+        .pos(0.f, 4.f)
         .parent(this)
+        .scaleY(.8f)
         .store(line);
+        
+    Build<CCSprite>::createSpriteName("checkpoint_01_001.png")
+        .pos(1.2f, 8.f)
+        .scale(.3f)
+        .parent(this)
+        .visible(false)
+        .store(practiceSprite);
 
     Build<GlobedSimplePlayer>::create(data)
         .opacity(forceOnTop ? 255 : static_cast<uint8_t>(settings.levelUi.progressOpacity * 255))
@@ -36,14 +44,19 @@ void PlayerProgressIcon::updateIcons(const PlayerIconData& data) {
         .store(playerIcon);
 }
 
-void PlayerProgressIcon::updatePosition(float progress) {
+void PlayerProgressIcon::updatePosition(float progress, bool isPracticing) {
     auto parent = this->getParent()->getParent();
     if (!parent) return;
 
     CCSize pbSize = parent->getScaledContentSize();
     float prOffset = (pbSize.width - 2.f) * progress;
 
-    this->toggleLine(progress > 0.01f && progress < 0.99f);
+    bool lineIsVisible = progress > 0.01f && progress < 0.95f;
+    bool practiceSpriteVisible = lineIsVisible && isPracticing;
+    
+    this->toggleLine(lineIsVisible);
+    this->togglePracticeSprite(practiceSpriteVisible);
+
     if (forceOnTop) {
         this->setZOrder(100000);
     } else {
@@ -54,6 +67,10 @@ void PlayerProgressIcon::updatePosition(float progress) {
 
 void PlayerProgressIcon::toggleLine(bool enabled) {
     line->setVisible(enabled);
+}
+
+void PlayerProgressIcon::togglePracticeSprite(bool enabled) {
+    practiceSprite->setVisible(enabled);
 }
 
 void PlayerProgressIcon::setForceOnTop(bool state) {
